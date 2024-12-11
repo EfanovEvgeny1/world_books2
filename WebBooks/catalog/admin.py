47971c1cb959cf4aba70c8e86281cce717259326
EfanovEvgeny1 <eugenye@bk.ru>
2024-12-11 14:22:28 +0300

    11.12.2024 Ефанов, Дубровина ЛЗ 19

--- WebBooks/catalog/admin.py
+++ WebBooks/catalog/admin.py
@@ -1,3 +1,10 @@
+from .models import Author, Book, Genre, Language, Publisher, Status, BookInstance
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Author) 
+admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(Language) 
+admin.site.register(Publisher)
+admin.site.register(Status)
+admin.site.register(BookInstance)
